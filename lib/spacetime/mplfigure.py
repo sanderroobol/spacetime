d6ad4595b717b1ba2531c9fdb645956450e71251
Sander Roobol <git@uithetblauw.nl>
2011-03-26 08:37:28 +0100

    keep Matplotlib toolbar from appearing on Windows

--- lib/spacetime/mplfigure.py
+++ lib/spacetime/mplfigure.py
@@ -1,7 +1,7 @@
 import matplotlib
 matplotlib.use('WXAgg')
 
-import matplotlib.figure, matplotlib.transforms, matplotlib.backends.backend_wx, matplotlib.backends.backend_wxagg
+import matplotlib.figure, matplotlib.transforms, matplotlib.backends.backend_wx, matplotlib.backends.backend_wxagg, matplotlib.backend_bases
 import wx
 
 from enthought.traits.api import Str
@@ -10,12 +10,27 @@ from enthought.traits.ui.basic_editor_factory import BasicEditorFactory
 
 
 class ModifiedToolbar(matplotlib.backends.backend_wx.NavigationToolbar2Wx):
-	def __init__(self, statuscallback, *args, **kwargs):
-		super(ModifiedToolbar, self).__init__(*args, **kwargs)
+	def __init__(self, canvas, statuscallback):
+		matplotlib.backend_bases.NavigationToolbar2.__init__(self, canvas)
+		self.canvas = canvas
+		self._idle = True
+		self.statbar = None
 		self.statuscallback = statuscallback
 
 	def set_message(self, s):
 		self.statuscallback(s)
+
+	def _init_toolbar(self):
+		self._NTB2_BACK    = None
+		self._NTB2_FORWARD = None
+		self._NTB2_PAN     = None
+		self._NTB2_ZOOM    = None
+
+	def ToggleTool(self, *args, **kwargs):
+		pass
+
+	def EnableTool(self, *args, **kwargs):
+		pass
 
 
 class _MPLFigureEditor(Editor):
@@ -32,7 +47,7 @@ class _MPLFigureEditor(Editor):
 
 	def _create_canvas(self, parent):
 		mpl_control = matplotlib.backends.backend_wxagg.FigureCanvasWxAgg(parent, -1, self.value)
-		self.value.toolbar = ModifiedToolbar(self.set_status, mpl_control)
+		self.value.toolbar = ModifiedToolbar(mpl_control, self.set_status)
 		return mpl_control
 
 	def set_status(self, s):
