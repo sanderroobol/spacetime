f96cce2887911c5dd6782a31152a70e6425e1447
Sander Roobol <git@uithetblauw.nl>
2011-06-22 22:23:01 +0200

    install licences and readme

--- win32/geninstaller.py
+++ win32/geninstaller.py
@@ -2,6 +2,51 @@ import sys, os
 sys.path.append(os.path.realpath('lib'))
 import spacetime.version
 
+class Installer(object):
+	def __init__(self):
+		self.install_commands = []
+		self.uninstall_files = []
+		self.uninstall_dirs = []
+
+	def install_recursively(self, srcpath, srcdir):
+		oldpath = os.getcwd()
+		os.chdir(srcpath)
+		srcpath = srcpath.replace('/', '\\')
+
+		for dirpath, dirnames, files in os.walk(srcdir):
+			dirpath = dirpath.replace('/', '\\')
+			self.uninstall_dirs.append(dirpath)
+
+			self.install_commands.append('  CreateDirectory "$INSTDIR\%s"' % dirpath)
+			self.install_commands.append('  SetOutPath "$INSTDIR\%s"' % dirpath)
+			for f in files:
+				extension = f.split('.')[-1]
+				if f.startswith('.') or extension in ('pyc', 'pyo', 'orig'):
+					continue
+				self.uninstall_files.append('%s\%s' % (dirpath, f))
+				if extension == 'py':
+					self.uninstall_files.append('%s\%sc' % (dirpath, f))
+					self.uninstall_files.append('%s\%so' % (dirpath, f))
+				self.install_commands.append('  File ..\%s\%s\%s' % (srcpath, dirpath, f))
+		os.chdir(oldpath)
+
+	def print_install(self):
+		print "\n".join(self.install_commands)
+
+	def print_uninstall(self):
+		for f in self.uninstall_files:
+			print '  Delete "$INSTDIR\%s"' % f
+
+		self.uninstall_dirs.reverse()
+		for d in self.uninstall_dirs:
+			print '  RMDir "$INSTDIR\%s"' % d
+
+
+installer = Installer()
+installer.install_recursively('lib', 'spacetime')
+installer.install_recursively('win32', 'licenses')
+
+
 print """
 ; Based on: NSIS Modern User Interface/Basic Example Script by Joost Verburg
 
@@ -17,9 +62,9 @@ print """
 """
 
 print """
-  Name "Spacetime #VERSION#"
-  OutFile "Spacetime-#VERSION#.exe"
-""".replace('#VERSION#', spacetime.version.version)
+  Name "Spacetime {version}"
+  OutFile "Spacetime-{version}.exe"
+""".format(version=spacetime.version.version)
 
 print """
   ;Default installation folder
@@ -62,6 +107,7 @@ Section
   File python-dist\python.exe
   File python-dist\pythonw.exe
   File python-dist\python26.dll
+  File ..\README.html
   File /r /x .* python-dist\Lib
 
   File debug.bat
@@ -69,25 +115,7 @@ Section
   ; AUTOMATICALLY GENERATED LIST OF FILES AND DIRS
 """
 
-uninstall_dirs = []
-uninstall_files = []
-os.chdir('lib')
-
-for dirpath, dirnames, files in os.walk('spacetime'):
-	dirpath = dirpath.replace('/', '\\')
-	uninstall_dirs.append(dirpath)
-
-	print '  CreateDirectory "$INSTDIR\%s"' % dirpath
-	print '  SetOutPath "$INSTDIR\%s"' % dirpath
-	for f in files:
-		extension = f.split('.')[-1]
-		if f.startswith('.') or extension in ('pyc', 'pyo', 'orig'):
-			continue
-		uninstall_files.append('%s\%s' % (dirpath, f))
-		if extension == 'py':
-			uninstall_files.append('%s\%sc' % (dirpath, f))
-			uninstall_files.append('%s\%so' % (dirpath, f))
-		print '  File ..\lib\%s\%s' % (dirpath, f)
+installer.print_install()
 
 print """
   ; END AUTOMATICALLY GENERATED LIST
@@ -120,6 +148,7 @@ Section "Uninstall"
   Delete "$INSTDIR\python.exe"
   Delete "$INSTDIR\pythonw.exe"
   Delete "$INSTDIR\python26.dll"
+  Delete "$INSTDIR\README.html"
   RMDir /r "$INSTDIR\Lib"
 
   Delete "$INSTDIR\debug.bat"
@@ -130,12 +159,7 @@ Section "Uninstall"
   ; AUTOMATICALLY GENERATED LIST OF FILES AND DIRS
 """
 
-for f in uninstall_files:
-	print '  Delete "$INSTDIR\%s"' % f
-
-uninstall_dirs.reverse()
-for d in uninstall_dirs:
-	print '  RMDir "$INSTDIR\%s"' % d
+installer.print_uninstall()
 
 print """
   ; END AUTOMATICALLY GENERATED LIST
