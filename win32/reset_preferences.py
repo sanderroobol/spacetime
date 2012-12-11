d4eda120908896e3de4fbf7ceabcc92cb32939a7
Sander Roobol <git@uithetblauw.nl>
2012-12-11 14:50:53 +0100

    Pretty WX dialog boxes for reset_preferences.py

--- win32/reset_preferences.py
+++ win32/reset_preferences.py
@@ -1,14 +1,18 @@
 import os
+import wx
 import spacetime.prefs
+
+app = wx.App()
 
 file = spacetime.prefs.get_prefs_path()
 if os.path.exists(file):
 	try:
 		os.remove(file)
 	except Exception as e:
-		print "Unable to reset preferences: {0}".format(e)
+		wx.MessageBox('Unable to reset preferences: {0}'.format(e), 'Spacetime', wx.OK | wx.ICON_ERROR)
 	else:
-		print "Preferences have been reset"
+		wx.MessageBox('Spacetime preferences have been reset.', 'Spacetime', wx.OK | wx.ICON_INFORMATION)
 else:
-	print "Could not find preferences file, nothing to reset"
-raw_input("Press enter to close this window")
+	wx.MessageBox('Could not find preferences file. Preferences have already been reset or Spacetime has never run before.', 'Spacetime', wx.OK | wx.ICON_EXCLAMATION)
+
+app.MainLoop()
