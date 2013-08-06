8e7c111b47b57eb94dfa78f28395ec8d5c4b14a4
Sander Roobol <git@uithetblauw.nl>
2013-08-06 12:11:34 +0200

    fix reset_preferences.py script and make a little more robust

--- win32/reset_preferences.py
+++ win32/reset_preferences.py
@@ -1,18 +1,22 @@
 import os
 import wx
-import spacetime.prefs
+import spacetime.util
 
-app = wx.App()
+app = wx.App(redirect=False)
 
-file = spacetime.prefs.get_prefs_path()
-if os.path.exists(file):
-	try:
-		os.remove(file)
-	except Exception as e:
-		wx.MessageBox('Unable to reset preferences: {0}'.format(e), 'Spacetime', wx.OK | wx.ICON_ERROR)
+try: # put in some extra effort to always show a wx.MessageBox
+	file = spacetime.util.get_persistant_path('preferences')
+	if os.path.exists(file):
+		try:
+			raise Exception()
+			os.remove(file)
+		except Exception as e:
+			wx.MessageBox('Unable to reset preferences: {0}'.format(e), 'Spacetime', wx.OK | wx.ICON_ERROR)
+		else:
+			wx.MessageBox('Spacetime preferences have been reset.', 'Spacetime', wx.OK | wx.ICON_INFORMATION)
 	else:
-		wx.MessageBox('Spacetime preferences have been reset.', 'Spacetime', wx.OK | wx.ICON_INFORMATION)
-else:
-	wx.MessageBox('Could not find preferences file. Preferences have already been reset or Spacetime has never run before.', 'Spacetime', wx.OK | wx.ICON_EXCLAMATION)
+		wx.MessageBox('Could not find preferences file. Preferences have already been reset or Spacetime has never run before.', 'Spacetime', wx.OK | wx.ICON_EXCLAMATION)
+except Exception as e:
+	wx.MessageBox('An unexpected error occurred: {0!r}'.format(e), 'Spacetime', wx.OK | wx.ICON_ERROR)
 
 app.MainLoop()
